795a71571eb850a2acc1f3884510d16c1f8d5bb2
JFumba <jessefumba@gmail.com>
2019-10-14 12:21:00 +0200

    empty project

---
+++ src/components/PasswordGenerator.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper">
+        <article class="passbox"></article>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
